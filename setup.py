fc5a9092126d85dae7435d6b98e080889a397b12
hades1232 <kimjuhyeok09@naver.com>
2022-03-06 17:01:12 +0900

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='mojang_python',
+   version='1.0.0',
+   description='Easy Mojang API Wrapper',
+   author='Hades_',
+   packages=['mojang_python'], 
+   install_requires=['requests', 'datetime'],
+)
