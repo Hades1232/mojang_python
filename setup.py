43de0782b79e09a7d7e799c655c312e459c0ae39
Hades <80930383+Hades1232@users.noreply.github.com>
2022-03-07 17:15:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
    name='mojang_python',
    version='1.0.0',
    description='Easy Mojang API Wrapper',
+   licence = 'Apache',
+   long_description=open('README.md').read(),
+   url = "https://github.com/Hades1232/mojang_python",
    author='Hades_',
    packages=['mojang_python'], 
    install_requires=['requests', 'datetime'],
+
 )
