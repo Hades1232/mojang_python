d371aed0206869b2f572ee2f05cf585a9d05c073
hades1232 <kimjuhyeok09@naver.com>
2023-04-02 19:08:31 +0900

    Version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='mojang_python',
-   version='1.0.0',
+   version='1.1.0',
    description='Easy Mojang API Wrapper',
    licence = 'Apache',
    long_description=open('README.md').read(),
@@ -10,7 +10,6 @@ setup(
    url = "https://github.com/Hades1232/mojang_python",
    author='Hades_',
    packages=['mojang_python'], 
-   install_requires=['requests', 'datetime'],
-   python_requires='>=3.5'
-
+   install_requires=['requests', 'typing'],
+   python_requires='>=3.10'
 )
